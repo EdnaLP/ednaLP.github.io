1f83a49489249fcd63fff512e6d8d481c2d5a056
EdnaLP <40434115+EdnaLP@users.noreply.github.com>
2020-06-16 15:14:19 +0200

    Add files via upload

---
+++ ubisoft/index_files/contact.js
@@ -0,0 +1,6 @@
+﻿(function ($) {
+    var heroSection = $('section.hero');
+    //heroSection.removeClass('hero--secondary').addClass('hero--tertiary');
+    $('.search-box, .hero__logo', heroSection).remove();
+    $('.hero__overlay', heroSection).removeClass('hero__overlay').addClass('hero__overlay--darker');
+})(jQuery);
